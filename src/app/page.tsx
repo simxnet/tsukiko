1776fa0ac4ce374bdbc028bda32423f291c356dc
simxnet <simxnet@envs.net>
2024-06-29 19:41:43 +0200

    fix: remove unused thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import NewSoundCard from "@/components/shared/new-sound-card";
 import SoundCard from "@/components/shared/sound-card";
-import VolumeSlider from "@/components/shared/volume-slider";
 import VolumeWrapper from "@/components/shared/volume-wrapper";
 import { Heading } from "@/components/ui/heading";
 import { Box, Grid, GridItem } from "@/styled-system/jsx";
-import { motion } from "framer-motion";
 import React from "react";
 
 export default function Page() {
